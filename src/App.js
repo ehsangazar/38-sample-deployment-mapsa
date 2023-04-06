2018e99c54b4fa14ab869129c985d9a51f11656f
Ehsan Gazar <me@ehsangazar.com>
2023-04-06 20:08:57 +1000

    init

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img
+          src={"https://persiajs.dev/_next/image?url=%2Flogo.png&w=128&q=75"}
+          className="App-logo"
+          alt="logo"
+        />
+        <a
+          className="App-link"
+          href="https://persiajs.dev"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn with Gazar 2
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
